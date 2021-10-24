3ea8cebc1155327d147fc11dd9ca9be8feb3ef93
Rishab Satyakaal <rishab.satyakaal@berkeley.edu>
2021-10-23 17:51:30 -0700

    working1

--- src/App.js
+++ src/App.js
@@ -1,36 +1,39 @@
-import {  useEffect, useState } from 'react';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
-import LiveText from './components/LiveText';
-import DifficultyBarChart from './components/DifficultyBarChart';
-import dummy from './data/dummy.json';
+import LiveText from "./components/LiveText";
+import DifficultyBarChart from "./components/DifficultyBarChart";
+import dummy from "./data/dummy.json";
 
 function App() {
-
-  const [currentTime, setCurrentTime] = useState(0);
+  const [text, setText] = useState("hi");
   const [data, setData] = useState(dummy);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      console.log('This will run every second!');
+      console.log("This will run every second!");
 
-      fetch('/text').then(res => res.json()).then(data => {
-        setCurrentTime(data.time);
-      });
+      fetch("/transcribe")
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data.lines);
+          setText(data.lines);
+        });
 
-      fetch('/data').then(res => res.json()).then(data => {
-        setData(data.values);
-      });
+      fetch("/data")
+        .then((res) => res.json())
+        .then((data) => {
+          setData(data.values);
+        });
     }, 1000);
     return () => clearInterval(interval);
   }, []);
 
-
   return (
     <div className="App">
       <div className="layout">
-        <LiveText width={20} text={currentTime}/>
-        <DifficultyBarChart data={data}/>
+        <LiveText width={20} text={text} />
+        <DifficultyBarChart data={data} />
       </div>
     </div>
   );
